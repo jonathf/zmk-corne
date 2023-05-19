6bb968bd1627686f12e3b3a635d34b4c5af7f949
Jonathan Feinberg <jonathf@gmail.com>
2023-05-19 22:06:44 +0200

    refactor norwegian translation

---
+++ config/definition.h
@@ -0,0 +1,41 @@
+// Norwegian translations
+
+#define _AA LBKT            // Å
+#define _ACUTE RA(EQUAL)
+#define _AE SQT             // Æ
+#define _AMPS LS(N6)
+#define _AT RA(N2)
+#define _BSLH EQUAL
+#define _CARET RBRC
+#define _COLON LS(DOT)
+#define _DOLLAR RA(N4)
+#define _DQT LS(N2)
+#define _EQUAL LS(N0)
+#define _EURO RA(E)
+#define _EXCL LS(N1)
+#define _FSLH LS(N7)
+#define _GRAVE LS(EQUAL)
+#define _GT RA(LS(X))
+#define _HASH LS(N3)
+#define _LBKT RA(N8)
+#define _LBRC RA(N7)
+#define _LDANG RA(Z)
+#define _LPAR LS(N8)
+#define _LT RA(LS(Z))
+#define _MINUS FSLH
+#define _OE SEMI            // Ø
+#define _PARA LS(GRAVE)
+#define _PIPE GRAVE
+#define _PLUS MINUS
+#define _PRCNT LS(N5)
+#define _QMARK UNDER
+#define _RBKT RA(N9)
+#define _RBRC RA(N0)
+#define _RDANG RA(X)
+#define _RPAR LS(N9)
+#define _SEMI LS(COMMA)
+#define _SQT BSLH
+#define _STAR PIPE
+#define _TILDE RA(RBKT)
+#define _UMLAUT RA(LBKT)
+#define _UNDER QMARK
