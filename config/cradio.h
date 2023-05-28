199badf68fd79b1b3d8cc953736e501dba03f91e
Jonathan Feinberg <jonathf@gmail.com>
2023-05-28 13:22:33 +0200

    all sticky

--- config/cradio.h
+++ config/cradio.h
@@ -1,16 +1,20 @@
+#include <behaviors.dtsi>
+#include <dt-bindings/zmk/keys.h>
+#include <dt-bindings/zmk/bt.h>
+#include <dt-bindings/zmk/ext_power.h>
+
+#include "no_nb.h"
+
 #define HOM_L 0
-#define CAP_L 1
-#define NUM_L 2
-#define NAV_L 3
-#define LOC_L 4
+#define NUM_L 1
+#define NAV_L 2
+#define LOC_L 3
 
 #define left_half 5 6 7 8 9 15 16 17 18 19 25 26 27 28 29 30 31 32 33
 #define right_half 0 1 2 3 4 10 11 12 13 14 20 21 22 23 24 30 31 32 33
 
 #define _m_space &lht LGUI SPACE
-#define _m_sspace &lht LGUI LS(SPACE)
 #define _m_enter &lht LGUI ENTER
-#define _m_tab &lht LGUI TAB
 
 #define top(q, w, e, r, t, y, u, i, o, p) \
   &kp q   &kp w         &kp e         &kp r         &kp t \
