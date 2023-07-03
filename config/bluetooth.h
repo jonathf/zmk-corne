a8971dfb4fb0c43a92e16d39158e738dc2a1cabd
Jonathan Feinberg <jonathf@gmail.com>
2023-07-03 23:47:35 +0200

    simpler structure

--- config/bluetooth.h
+++ config/bluetooth.h
@@ -1,13 +1,10 @@
 #include "helper.h"
 
-ZMK_BEHAVIOR(_bt_clear_0, macro,
-  bindings = <&to 0>, <&bt BT_SEL 0>, <&bt BT_CLR>;
-)
 ZMK_BEHAVIOR(_BT0, hold_tap,
   bindings = "tap-preferred";
   tapping-term-ms=<500>;
   quick-tap-ms=<400>;
   global-quick-tap;
   hold-trigger-on-release;
-  bindings = <&bt BT_SEL 0>, <&_bt_clear_0>;
+  bindings = <&bt BT_SEL 0>, <&bt BT_CLR>;
 )
