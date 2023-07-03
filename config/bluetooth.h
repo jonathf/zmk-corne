05fcd74480631d8c1c98d53b3dd67218d8601f84
Jonathan Feinberg <jonathf@gmail.com>
2023-07-03 23:41:45 +0200

    one bluetooth device

--- config/bluetooth.h
+++ config/bluetooth.h
@@ -1,27 +1,13 @@
 #include "helper.h"
 
-#define _bt_clear(n) \
-  ZMK_BEHAVIOR(_bt_clear_ ## n, macro, \
-      bindings = <&to 0>, <&bt BT_SEL n>, <&bt BT_CLR>; \
-  )
-
-#define _bt_key(n) \
-  ZMK_BEHAVIOR(_BT ## n, hold_tap, \
-      bindings = "tap-preferred"; \
-      tapping-term-ms=<500>; \
-      quick-tap-ms=<400>; \
-      global-quick-tap; \
-      hold-trigger-on-release; \
-      bindings = <&bt BT_SEL n>, <&_bt_clear_ ## n>; \
-  )
-
-// _bt_clear(0)
-// _bt_key(0)
-// _bt_clear(1)
-// _bt_key(1)
-// _bt_clear(2)
-// _bt_key(2)
-// _bt_clear(3)
-// _bt_key(3)
-// _bt_clear(4)
-// _bt_key(4)
+ZMK_BEHAVIOR(_bt_clear_0, macro,
+  bindings = <&to 0>, <&bt BT_SEL 0>, <&bt BT_CLR>;
+)
+ZMK_BEHAVIOR(_BT0, hold_tap,
+  bindings = "tap-preferred";
+  tapping-term-ms=<500>;
+  quick-tap-ms=<400>;
+  global-quick-tap;
+  hold-trigger-on-release;
+  bindings = <&bt BT_SEL 0>, <&_bt_clear_0>;
+)
