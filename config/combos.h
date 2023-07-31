9428aaa1596c5bdb19a67a5ba94f228f7b87f5b8
Jonathan Feinberg <jonathf@gmail.com>
2023-07-31 14:57:14 +0200

    new combos

--- config/combos.h
+++ config/combos.h
@@ -1,13 +1,20 @@
+/*
+      ╭────┬────┬────┬────╮  ╭────┬────┬────┬────╮
+ 0    1    2    3    4    5  6    7    8    9   10   11
+                                   INS  DEL
+12   13   14   15   16   17 18   19   20   21   22   23
+       LALT ESC  TAB              ENTER BKSP RALT
+24   25   26   27   28   29 30   31   32   33   34   35
+      ╰────┴────┴────┴────╯  ╰────┴────┴────┴────╯
+               36   37   38 39   40   41
+*/
 #include "helper.h"
-#include "36keys.h"
 
-ZMK_COMBO(_LALT, &sk LALT, LM3 LM4, L0)
-ZMK_COMBO(_RALT, &sk RALT, RM3 RM4, L0)
-
-ZMK_COMBO(_ESC, &kp ESC, LT3 LT2, L0)
-ZMK_COMBO(_TAB, &kp TAB, LB3 LB2, L0)
-ZMK_COMBO(_INS, &kp INS, RT2 RT1, L0)
-
-ZMK_COMBO(_BKSP, &kp BKSP, RM3 RM2, L0)
-ZMK_COMBO(_DEL, &kp DEL, RM4 RM3, L0)
-ZMK_COMBO(_ENTER, &kp ENTER, RB3 RB2, L0)
+ZMK_COMBO(_BKSP, &kp BKSP, 20 21, 0)
+ZMK_COMBO(_DEL, &kp DEL, 8 9, 0)
+ZMK_COMBO(_ENTER, &kp ENTER, 19 20, 0)
+ZMK_COMBO(_ESC, &kp ESC, 14 15, 0)
+ZMK_COMBO(_INS, &kp INS, 7 8, 0)
+ZMK_COMBO(_LALT, &sk LALT, 13 14, 0)
+ZMK_COMBO(_RALT, &sk RALT, 21 22, 0)
+ZMK_COMBO(_TAB, &kp TAB, 15 16, 0)
