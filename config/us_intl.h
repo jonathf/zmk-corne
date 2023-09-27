54975445311fc61790275838cf109e42f11ed0ef
Jonathan Feinberg <jonathf@gmail.com>
2023-09-27 16:36:29 +0200

    attempt at unifying mac

--- config/us_intl.h
+++ config/us_intl.h
@@ -1,7 +1,7 @@
 // Symbol definition in US-International
 
 #define __AA RA(W)            // Å
-#define __ACUTE ACUTE
+#define __ACUTE RA(SQT)
 #define __AE RA(Z)             // Æ
 #define __AMPS AMPS
 #define __AT AT
@@ -11,7 +11,7 @@
 #define __DOLLAR DOLLAR
 #define __DQT DQT
 #define __EQUAL EQUAL
-#define __EURO EURO
+#define __EURO RA(5)
 #define __EXCL EXCL
 #define __FSLH FSLH
 #define __GRAVE GRAVE
@@ -19,23 +19,23 @@
 #define __HASH HASH
 #define __LBKT LBKT
 #define __LBRC LBRC
-#define __LDANG LDANG
+#define __LDANG RA(LBKT)
 #define __LPAR LPAR
 #define __LT LT
 #define __MINUS MINUS
 #define __OE RA(L)            // Ø
-#define __PARA PARA
+#define __PARA RA(LS(S))
 #define __PIPE PIPE
 #define __PLUS PLUS
 #define __PRCNT PRCNT
 #define __QMARK QMARK
 #define __RBKT RBKT
 #define __RBRC RBRC
-#define __RDANG RDANG
+#define __RDANG RA(RBKT)
 #define __RPAR RPAR
 #define __SEMI SEMI
 #define __SQT SQT
 #define __STAR STAR
 #define __TILDE TILDE
-#define __UMLAUT UMLAUT
+#define __UMLAUT RA(DQT)
 #define __UNDER UNDER
