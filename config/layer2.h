101ef3f189e0a335461de8cc4e874f117417fe51
Jonathan Feinberg <jonathf@gmail.com>
2023-11-18 22:13:08 +0100

    dual hexmac and nomac

--- config/layer2.h
+++ config/layer2.h
@@ -1,13 +1,20 @@
 // HOST_OS: 0 iso, 2 macos unicode
 
 #define _0_EXCL &kp EXCL
+#define _1_EXCL &kp LS(N1)
 #define _0_AT &kp AT
+#define _1_AT &kp BSLH
 #define _0_LBRC &kp LBRC
+#define _1_LBRC &kp LA(LS(N8))
 #define _0_RBRC &kp RBRC
+#define _1_RBRC &kp LA(LS(N9))
 #define _0_EQUAL &kp EQUAL
+#define _1_EQUAL &kp LS(N0)
 
 ZMK_PAIR(__0_PLUS, PLUS, F12)
 #define _0_PLUS &__0_PLUS
+ZMK_PAIR(__1_PLUS, MINUS, F12)
+#define _1_PLUS &__1_PLUS
 
 ZMK_PAIR(__N7, N7, F7)
 #define _N7 &__N7
@@ -18,47 +25,57 @@ ZMK_PAIR(__N8, N8, F8)
 ZMK_PAIR(__N9, N9, F9)
 #define _N9 &__N9
 
+#define _1_AA &kp LBRC
 ZMK_UNICODE_PAIR(__2_AA, N0, N0, E, N5, N0, N0, C, N5)
 #define _2_AA &__2_AA
 
 #define _0_STAR &kp STAR
+#define _1_STAR &kp LS(BSLH)
 #define _0_DQT &kp DQT
+#define _1_DQT &kp LS(N2)
 #define _0_LPAR &kp LPAR
+#define _1_LPAR &kp LS(N8)
 #define _0_RPAR &kp RPAR
+#define _1_RPAR &kp LS(N9)
 #define _0_DOLLAR &kp DOLLAR
+#define _1_DOLLAR &kp LS(N4)
 
 ZMK_PAIR(__0_MINUS, MINUS, F11)
 #define _0_MINUS &__0_MINUS
+ZMK_PAIR(__1_MINUS, FSLH, F11)
+#define _1_MINUS &__1_MINUS
 
 ZMK_PAIR(__N4, N4, F4)
 #define _N4 &__N4
-
 ZMK_PAIR(__N5, N5, F5)
 #define _N5 &__N5
-
 ZMK_PAIR(__N6, N6, F6)
 #define _N6 &__N6
 
+#define _1_OE &kp SEMI
 ZMK_UNICODE_PAIR(__2_OE, N0, N0, F, N8, N0, N0, D, N8)
 #define _2_OE &__2_OE
 
 #define _0_PRCNT &tt RALT PRCNT
+#define _1_PRCNT &tt RALT LS(N5)
 #define _0_CARET &tt LALT CARET
+#define _1_CARET &tt LALT LS(RBRC)
 #define _0_LBKT &tt LCTRL LBKT
+#define _1_LBKT &tt LCTRL LA(N8)
 #define _0_RBKT &tt LSHIFT RBKT
+#define _1_RBKT &tt LSHIFT LA(N9)
 #define _0_UNDER &tt LGUI UNDER
+#define _1_UNDER &tt LGUI LS(FSLH)
 
 ZMK_PAIR(__N0, N0, F10)
 #define _N0 &__N0
-
 ZMK_PAIR(__N1, N1, F1)
 #define _N1 &__N1
-
 ZMK_PAIR(__N2, N2, F2)
 #define _N2 &__N2
-
 ZMK_PAIR(__N3, N3, F3)
 #define _N3 &__N3
 
+#define _1_AE &kp SQT
 ZMK_UNICODE_PAIR(__2_AE, N0, N0, E, N6, N0, N0, C, N6)
 #define _2_AE &__2_AE
--- config/layer3.h
+++ config/layer3.h
@@ -1,17 +1,23 @@
 // HOST_OS: 0 iso, 2 macos unicode
 
 #define _0_PIPE &kp PIPE
+#define _1_PIPE &kp LA(N7)
 
+#define _1_PARA &kp GRAVE
 ZMK_UNICODE_SINGLE(__2_PARA, N0, N0, A, N7)
 #define _2_PARA &__2_PARA
 
+#define _1_LDANG &kp LS(LA(V))
 ZMK_UNICODE_SINGLE(__2_LDANG, N0, N0, A, B)
 #define _2_LDANG &__2_LDANG
 
+#define _1_RDANG &kp LS(LA(B))
 ZMK_UNICODE_SINGLE(__2_RDANG, N0, N0, B, B)
 #define _2_RDANG &__2_RDANG
 
 #define _0_QMARK &kp QMARK
+#define _1_QMARK &kp UNDER
+
 #define _HOME &kp HOME
 #define _PG_DN &kp PG_DN
 #define _PG_UP &kp PG_UP
@@ -19,10 +25,16 @@ ZMK_UNICODE_SINGLE(__2_RDANG, N0, N0, B, B)
 #define _PSCRN &kp PSCRN
 
 #define _0_HASH &kp HASH
+#define _1_HASH &kp LS(N3)
 #define _0_SQT &kp SQT
+#define _1_SQT &kp GRAVE
 #define _0_LT &kp LT
+#define _1_LT &kp LA(V)
 #define _0_GT &kp GT
+#define _1_GT &kp LA(B)
 #define _0_AMPS &kp AMPS
+#define _1_AMPS &kp LS(N6)
+
 #define _LEFT &kp LEFT
 #define _DOWN &kp DOWN
 #define _UP &kp UP
@@ -31,10 +43,17 @@ ZMK_UNICODE_SINGLE(__2_RDANG, N0, N0, B, B)
 // nop backspace
 // nop percent
 
+#define _1_EURO &kp LA(N4)
 ZMK_UNICODE_SINGLE(__2_EURO, N2, N0, A, C)
 #define _2_EURO &__2_EURO
+
 #define _0_TILDE &kp TILDE
+#define _1_TILDE &kp LA(RBKT)
+
 #define _0_GRAVE &kp GRAVE
+#define _1_GRAVE &kp LS(EQUAL)
+
 #define _0_ACUTE &kp ACUTE
+#define _1_ACUTE &kp EQUAL
 ZMK_UNICODE_SINGLE(__2_ACUTE, N0, N0, B, N4)
 #define _2_ACUTE &__2_ACUTE
