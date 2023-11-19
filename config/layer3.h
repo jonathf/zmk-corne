2466973082348213ee809793ebb0472a2bb03393
Jonathan Feinberg <jonathf@gmail.com>
2023-11-19 23:12:03 +0100

    tilde???

--- config/layer3.h
+++ config/layer3.h
@@ -49,8 +49,9 @@ ZMK_UNICODE_SINGLE(__2_EURO, N2, N0, A, C)
 
 #define _0_TILDE &hold_tap LALT TILDE
 // #define _1_TILDE &hold_tap LALT LA(RBRC)
-ZMK_MOD_NODEAD(_tilde, LA(RBRC))
-#define _1_TILDE &_tilde LALT 0
+// ZMK_MOD_NODEAD(_tilde, LA(RBRC))
+// #define _1_TILDE &_tilde LALT 0
+#define _1_TILDE &kp RBRC
 
 #define _0_GRAVE &kp GRAVE
 // #define _1_GRAVE &kp LS(EQUAL)
