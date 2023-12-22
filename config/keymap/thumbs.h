06b91b81c67e2be2861ce3b7420701d6b3c3575b
Jonathan Feinberg <jonathf@gmail.com>
2023-12-22 13:53:54 +0100

    better thumb combos

--- config/keymap/thumbs.h
+++ config/keymap/thumbs.h
@@ -12,11 +12,13 @@
 ZMK_BEHAVIOR(__0_capsword, caps_word,
     continue-list = <UNDER MINUS BKSP DEL>;
 )
-#define _0_CAPS &__0_capsword
+ZMK_HOLDTAP(__0_layer_caps, sl, __0_capsword)
+#define _0_CAPS &__0_layer_caps 3 0
 
 ZMK_BEHAVIOR(__1_capsword, caps_word,
     continue-list = <FSLH LS(FSLH) BKSP DEL>;
 )
-#define _1_CAPS &__1_capsword
+ZMK_HOLDTAP(__1_layer_caps, sl, __1_capsword)
+#define _1_CAPS &__1_layer_caps 3 0
 
 #define _SPACE &hold_tap LS(LC(LGUI)) SPACE
