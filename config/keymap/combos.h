41fff37dd4d52b8ed6e9d0433f76033d0bb3c1b8
Jonathan Feinberg <jonathf@gmail.com>
2023-12-29 12:58:17 +0100

    fix q combo

--- config/keymap/combos.h
+++ config/keymap/combos.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define _combo_q(layer, key) ZMK_COMBO(_Q_ ## layer, key, _UL4 _UL4, layer)
+#define _combo_q(layer, key) ZMK_COMBO(_Q_ ## layer, key, _UL3 _UL4, layer)
 _combo_q(0, _Q)
 _combo_q(1, _1_EXCL)
 _combo_q(2, _1_PIPE)
